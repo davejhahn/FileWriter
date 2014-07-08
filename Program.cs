978b85ceb4c76cecfcbb7332d48544522887d24a
Dave Hahn <davejahahn@gmaillcom>
2014-07-08 08:11:16 -0500

    Created application and initial check-in

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 2)
+            {
+                Console.WriteLine("Usage:  FileWriter <FileName> \"Message\"");
+                return;
+            }            
+
+            string filename = args[0];
+            string message = args[1];
+
+            StreamWriter sw = new StreamWriter(filename);
+            sw.WriteLine(message);
+            sw.Flush();
+            sw.Close();
+            return;
+
+        }
+    }
+}
